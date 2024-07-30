62169f724bde6f3d6cd214f73f77369ace03e06c
Christian <125867560+CPerich@users.noreply.github.com>
2024-07-30 18:06:42 +0000

    Edit

---
+++ settings.js
@@ -0,0 +1,17 @@
+{
+    "workbench.startupEditor": "newUntitledFile",
+    "editor.suggestSelection":"first",
+    "vsintellicode.modify.editor.suggestSelection": "automaticallyOverrodeDefaultValue",
+    "window.zoomlevel": -1,
+    "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe",
+    "terminal.integrated.env.windows": {
+    "CHERE_INVOKING": "1"
+    },
+"terminal.integrated.shell.windows": [
+    "-1"
+],
+// optional add git-patch to use this version of git for build in visual studio model
+"git.patch": "C:\Program Files\\Git\\bin\\bash.exe"
+ }
+}
+
