38e05d612e147942e8c574369ee62df1f62f7dcf
Christian <125867560+CPerich@users.noreply.github.com>
2024-07-31 16:45:48 +0000

    Edit

--- all_checks.py
+++ all_checks.py
@@ -14,13 +14,13 @@ percent_free = 100 * du.free / du.total
 gigabytes_free = du.free / du.total
 if percent_free < min_percent or gigabytes_free < min_gb:
     return True
-return False
+    return False
 
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if check_disk_full(disk="/", min_gb=2, min_percent=10)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print("Disk full.")
         sys.exit(1)
 
@@ -28,3 +28,5 @@ print("Everything ok.")
 sys.ext(0)
 
 main()
+
+
